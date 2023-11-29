976f1c4db7bd789205990218f4e5388e106eb36f
francis <clyderitchie@yahoo.com>
2023-11-28 20:55:49 -0800

    Cleaned up the login and sign up page's styling

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,6 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
-      // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
@@ -14,7 +13,6 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        // If successful, redirect the browser to the profile page
         document.location.replace('/profile');
       } else {
         alert(response.statusText);
