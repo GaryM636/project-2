aba2b2e1ebee94c1d3abb6e580293087c3cb15d4
venasven <honhonlem@gmail.com>
2023-11-28 20:04:52 -0800

    added js to public folder and added user routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,4 @@
 DROP DATABASE IF EXISTS condensation_db;
 CREATE DB IF NOT EXISTS condensation_db;
+
+USE condensation_db;
---
+++ public/js/profile.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const  = document.querySelector('#').value.trim();
+    const  = document.querySelector('#').value.trim();
+    const  = document.querySelector('#').value.trim();
+  
+    if ( &&  && ) {
+      const response = await fetch(`/api/`, {
+        method: 'POST',
+        body: JSON.stringify({ , ,  }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/s/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete ');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new--form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.-list')
+    .addEventListener('click', delButtonHandler);
+  
