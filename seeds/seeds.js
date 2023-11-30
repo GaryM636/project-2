f87c19b338961fe87536091ad00b3709b18f849c
francis <clyderitchie@yahoo.com>
2023-11-29 21:13:09 -0800

    Worked on profile handlebar

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,27 +1,26 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Posts } = require('../models');
 
-const userData = [
-  {
-    userName: 'john_doe',
-    name: 'John Doe',
-    email: 'john@example.com',
-    password: 'Password1',
-  },
-  {
-    userName: 'jane_smith',
-    name: 'Jane Smith',
-    email: 'jane@example.com',
-    password: 'Password1',
-  },
-];
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+// Reworked the seeds so that the posts would work better for me and it loads faster when npm run seed is run
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
 
-const seedUsers = async () => {
-  await sequelize.sync ({ force: true }) 
-  await User.bulkCreate(userData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-};
 
-seedUsers();
+  let posts = [];
+
+  for (const post of postData) {
+    posts.push(await Posts.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    }));
+  }
+  process.exit(0);
+}
+
+seedDatabase();
