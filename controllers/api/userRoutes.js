672275b004c432ec5cb95d26c18a4a65080ab7d2
GaryM636 <142124124+GaryM636@users.noreply.github.com>
2023-12-05 20:39:51 -0800

    merging main

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const multer  = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage,})
+
+
 
 // /api/users
 
