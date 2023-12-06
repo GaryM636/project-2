c20fa907d3c8340cfa20d307d187e1d33e24da85
amanda-er <amanda.er991@gmail.com>
2023-12-05 22:16:21 -0800

    delete feature

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Posts } = require('../../models');
-
+const withAuth = require("../../utils/auth")
 // /api/posts
 
 // Creates a post
@@ -17,5 +17,20 @@ router.post('/', async (req, res) => {
         res.status(500).json(err.message);
     }
 });
+
+router.delete("/:id", withAuth, async (req, res) => {
+    try {
+     const dbRes = await Posts.destroy({
+        where: {
+            id: req.params.id
+        }
+     })   
+
+     res.status(200).json(dbRes);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err.message);  
+    }
+})
 
 module.exports = router;  
--- public/js/profile.js
+++ public/js/profile.js
@@ -40,8 +40,9 @@ formData.append('bio', bio);
   const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
+      console.log(id);
   
-      const response = await fetch(`/api/s/${id}`, {
+      const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE',
       });
   
@@ -57,7 +58,8 @@ formData.append('bio', bio);
     .querySelector('#profile-form')
     .addEventListener('submit', newFormHandler);
   
-  // document
-  //   .querySelector('.-list')
-  //   .addEventListener('click', delButtonHandler);
+   document
+   .querySelectorAll('.delete-post-btn')
+   .forEach(button => button.addEventListener('click', delButtonHandler))
+    
   
